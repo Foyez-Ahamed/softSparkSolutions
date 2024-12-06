68ad3e5735ee4ee3d90eaccb3b6e66aa14a68039
Foyez-Ahamed <foyez1435@gmail.com>
2024-12-07 01:02:32 +0600

    basic props drilling practice

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,13 @@ function App() {
 
    <h1 className='text-3xl font-bold underline text-center'>Hello world</h1>
 
-   <img className='w-[600px] mt-5 mx-auto rounded-md' src={image} alt={description} />
+   <img className='w-[100px] h-[100px] mt-5 mx-auto rounded-md' src={image} alt={description} />
 
    <ChildComponet/>
+
+   <Device name="Mobile" price="20000" />
+   <Device name="Laptop" price="50000"/>
+   <Device name="Tablet" price="30000"/>
 
    </>
   )
@@ -28,6 +32,15 @@ function ChildComponet(){
     age : 26
   }
 
+  // style by object //
+  const descriptionStyle = {
+    border: '2px solid purple',
+    padding: '10px',
+    maxWidth: '70%',
+    margin: '20px auto'
+  }
+  
+
   return(
     <div className='mt-5 text-center'>
 
@@ -35,7 +48,30 @@ function ChildComponet(){
       <h1>Assalamualaikum, This is {mySelf.name}. My Father name is {mySelf.fatherName}. My mother name is {mySelf.motherName}. My age is {mySelf.age}.</h1>
       </div>
 
+      <div className='mt-5' style={descriptionStyle}>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum aliquam molestiae alias sapiente nisi, optio sit voluptatibus explicabo quos voluptatem nobis a veritatis cupiditate quo hic perferendis, atque, ipsum sequi quia maxime repellendus est soluta animi. Temporibus ad asperiores neque?</p>
+      </div>
+
     </div>
+  )
+}
+
+// function for basic props(properties) drilling //
+
+function Device(props){
+
+  // console.log(props);
+
+  const {name, price} = props; // object destructuring // 
+
+  return(
+    <>
+    <div>
+      {/* <p className='text-center'> This device name is : {props?.name}. This device price is : {props?.price}</p> */}
+
+      <p className='text-center'> This device name is : {name}. This device price is : {price}</p>
+    </div>
+    </>
   )
 }
 
